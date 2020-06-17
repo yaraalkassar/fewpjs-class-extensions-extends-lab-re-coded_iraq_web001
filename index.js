5d68da939643365586b121971898f56113b80017
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-17 13:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ return sum;
 }
 
 class Triangle extends Polygon {
-  constructor() {
+get isValid(){
+  let side1 = this.sides[0]
+   let side2 = this.sides[1]
+   let side3 = this.sides[2]
+ 
+   if (side1 + side2 > side3 && side1+side3>side2 && side2+side3>side1){
+     return true
+   }else{
+     return false
+   }
 
-  }
+}
 }
