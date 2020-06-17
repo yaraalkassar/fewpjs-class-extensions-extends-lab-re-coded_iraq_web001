3f4cfccd83f6f9eee28976b150e6fd620eea38d0
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-17 13:17:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ get isValid(){
   let side1 = this.sides[0]
    let side2 = this.sides[1]
    let side3 = this.sides[2]
- 
+
    if (side1 + side2 > side3 && side1+side3>side2 && side2+side3>side1){
      return true
    }else{
